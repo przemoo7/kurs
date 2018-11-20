d5bde51906b8d601a69a8c7c7a96b8f0c14ae537
przemoo7 <44892127+przemoo7@users.noreply.github.com>
2018-11-20 10:20:07 +0100

    SeleniumGridExampleTest

---
+++ src/test/java/tests/SeleniumGridExampleTest.java
@@ -0,0 +1,44 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.testng.Assert.assertTrue;
+
+public class SeleniumGridExampleTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest() throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://10.110.13.129:44822/wd/hub"), capability);
+    }
+
+    @Test
+    public void searchQueryInGoogle() {
+        driver.navigate().to("http://google.pl");
+
+        driver.findElement(By.name("q")).sendKeys("JavaStart");
+        driver.findElement(By.name("q")).submit();
+
+        String pageTitle = driver.getTitle();
+
+        assertTrue(pageTitle.contains("JavaStart"));
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        driver.close();
+        driver.quit();
+    }
+}
